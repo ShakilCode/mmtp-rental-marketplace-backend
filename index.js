90d059fdd765459b666707cb575209bcc6048e5d
ShakilCode <shakilahamedya0108@gmail.com>
2026-04-19 16:34:35 +0530

    Setup Express server and connect MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hello World")
+// Import the express.js framework
+import express from 'express';
+// Import the dotenv
+import dotenv from 'dotenv'
+// Import the mongoose to connect backend to the MongoDB
+import mongoose from 'mongoose'
+
+// reads .env file and loads values into process.env
+dotenv.config()
+
+// MongoDB URI
+const mongoURI = process.env.MONGO_URI
+
+// connect our backend with the MongoDB
+mongoose.connect(mongoURI).then(
+    ()=>{
+        console.log("Connected to MongoDB")
+    }
+).catch(
+    ()=>{
+        console.log("Error connecting to MongoDB")
+    }
+)
+
+// Assign the express backend to a variable 
+const app = express()
+
+// Body Parser (middleware)
+app.use(express.json())
+
+// Start the server and make it listed for incoming requests on a specified port
+app.listen(process.env.PORT,
+    // Arrow function
+    ()=>{
+        console.log("Server is running perfectly")
+    }
+)
